cf2f497421e5746483dbab7ebd6cdf710111e587
Alex Sartori <alex.sartori1997@gmail.com>
2020-09-04 17:30:25 +0200

    Create MarkovChain class

---
+++ LoRaSim/MarkovChain.py
@@ -0,0 +1,48 @@
+'''
+    MarkovChain.py - Represents a Markov chain probability matrix
+    with additional attributes (title & description). Data can be
+    loaded from a file in .ini format. Any unspecified probability
+    will be zero.
+    Created by Alessandro Sartori, August 2020
+'''
+
+
+class MarkovChain:
+    def __init__(self):
+        self.title = ''
+        self.description = ''
+        self.p00 = 0
+        self.p01 = 0
+        self.p10 = 0
+        self.p11 = 0
+
+    def loadFromFile(self, fname):
+        for line in open(fname):
+            self._parseKeyValueString(line.strip())
+
+    def _parseKeyValueString(self, line):
+        args = line.split('=', 1)
+
+        if len(args) != 2:
+            raise SyntaxError("Not in key=value format: " + line)
+
+        key = args[0].lower().strip()
+        val = args[1].strip()
+
+        self._parseKeyValuePair(key, val)
+
+    def _parseKeyValuePair(self, k, v):
+        if k == 'p00':
+            self.p00 = float(v)
+        elif k == 'p01':
+            self.p01 = float(v)
+        elif k == 'p10':
+            self.p10 = float(v)
+        elif k == 'p11':
+            self.p11 = float(v)
+        elif k == 'title':
+            self.title = v
+        elif k == 'description':
+            self.description = v
+        else:
+            raise SyntaxError("Unknown key " + k + " for value " + v)
---
+++ test/test_MarkovChain.py
@@ -0,0 +1,52 @@
+import os
+from MarkovChain import MarkovChain
+
+
+def testInit():
+    chain = MarkovChain()
+    assert chain.p00 == 0
+    assert chain.p01 == 0
+    assert chain.p10 == 0
+    assert chain.p11 == 0
+    assert chain.title == ''
+    assert chain.description == ''
+
+
+def testAttributes():
+    chain = MarkovChain()
+    chain.p00 = 0.8
+    chain.p01 = 0.2
+    chain.p10 = 0.37
+    chain.p11 = 0.63
+    chain.title = 'test_title'
+    chain.description = 'test_desc'
+
+    assert chain.p00 == 0.8
+    assert chain.p01 == 0.2
+    assert chain.p10 == 0.37
+    assert chain.p11 == 0.63
+    assert chain.title == 'test_title'
+    assert chain.description == 'test_desc'
+
+
+def testLoadFromFile():
+    chain = MarkovChain()
+    fname = 'test_chain.ini'
+
+    with open(fname, 'w') as f:
+        f.write('title=DR2\n')
+        f.write('description=Datarate #2: SF=10 and BW=125kHz\n')
+        f.write('p00=0.80\n')
+        f.write('p01=0.20\n')
+        f.write('p10=0.37\n')
+        f.write('p11=0.63\n')
+
+    chain.loadFromFile(fname)
+    os.remove(fname)
+
+    assert chain.p00 == 0.8
+    assert chain.p01 == 0.2
+    assert chain.p10 == 0.37
+    assert chain.p11 == 0.63
+    assert chain.title == 'DR2'
+    assert chain.description == 'Datarate #2: SF=10 and BW=125kHz'
