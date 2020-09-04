c401dedc6a582c0fe9986695fe0a036c9b816bdc
Alex Sartori <alex.sartori1997@gmail.com>
2020-09-04 18:25:29 +0200

    Add home toolbar

--- LoRaSim/gui/HomeWindow.py
+++ LoRaSim/gui/HomeWindow.py
@@ -4,5 +4,21 @@ from PyQt5 import QtWidgets, QtGui, QtCore
 class HomeWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(QtWidgets.QMainWindow, self).__init__()
-        self.setWindowTitle("LoRaSim")
-        self.setMinimumSize(self.sizeHint())
+        self.setWindowTitle("LoRaSim - Home")
+        self.setMinimumSize(400, 600)
+        self.initUI()
+
+    def initUI(self):
+        self.toolbar = self.createToolbar()
+
+    def createToolbar(self):
+        startAction = QtWidgets.QAction(
+            QtGui.QIcon.fromTheme('media-playback-start'),
+            'Start', self
+        )
+        # startAction.triggered.connect(...)
+
+        toolbar = self.addToolBar('HomeToolbar')
+        toolbar.setToolButtonStyle(QtCore.Qt.ToolButtonTextBesideIcon)
+        toolbar.addAction(startAction)
+        return toolbar
