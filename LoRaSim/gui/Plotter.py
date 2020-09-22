771625c1c85e511969549704da6d5ecd810e37eb
Alex Sartori <alex.sartori1997@gmail.com>
2020-09-22 21:14:07 +0200

    Add 95% CI to plot

--- LoRaSim/gui/Plotter.py
+++ LoRaSim/gui/Plotter.py
@@ -1,3 +1,4 @@
+import math
 import matplotlib.pyplot as plt
 from LoRaSim.SimIntervals import Interval
 
@@ -7,11 +8,13 @@ class Plotter:
         assert isinstance(intervals, list)
         assert isinstance(intervals[0], tuple)
         assert isinstance(intervals[0][0], Interval)
+        assert isinstance(intervals[0][1], list)
         self.intervals = intervals
 
     def show_plot(self):
+        x_vals = []
         p_succ_y = []
-        p_succ_x = []
+        ci_min, ci_max = [], []
         succ, tot = 0, 0
 
         for interval in self.intervals:
@@ -19,21 +22,33 @@ class Plotter:
             data = interval[1]
             model = metadata.model
 
-            plt.vlines(metadata.start_time, 0, 1, linestyle=':', color='gray', linewidth=2)
-            plt.text(metadata.start_time + 10, 0.9, model.title, color='gray')
+            if len(self.intervals) > 1:
+                plt.vlines(metadata.start_time, 0, 1, linestyle=':', color='gray', linewidth=2)
+                plt.text(metadata.start_time, 1.1, model.title, fontsize=12, color='gray')
 
             for sample in data:
                 if sample[1]:
                     succ += 1
                 tot += 1
-                p_succ_y.append(succ/tot)
 
-            colors = ['lime' if i[1] else 'r' for i in data]
-            x = [i[0] for i in data]
-            plt.scatter(x, [0]*len(x), c=colors, marker='|')
-            p_succ_x.extend(x)
+                p = succ/tot
+                p_succ_y.append(p)
+                ci = 1.96*math.sqrt(succ*(tot-succ))/(tot*math.sqrt(tot))
+                ci_max.append(p+ci)
+                ci_min.append(p-ci)
 
-        plt.plot(p_succ_x, p_succ_y)
+            x_vals.extend(i[0] for i in data)
+            self._plot_recv_rate(data)
+
+        plt.fill_between(x_vals, ci_min, ci_max, color='orange', alpha=0.4, label='95% CI')
+        plt.plot(x_vals, p_succ_y, label='Success probability')
         plt.xlabel("Time (ms)")
         plt.ylabel("Success probability")
+        plt.legend()
+        plt.tight_layout()
+        # plt.ylim([0, 1])
         plt.show()
+
+    def _plot_recv_rate(self, data):
+        colors = ['lime' if i[1] else 'r' for i in data]
+        plt.scatter(list(i[0] for i in data), [0]*len(data), c=colors, marker='|')
